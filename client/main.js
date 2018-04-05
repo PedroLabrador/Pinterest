a4f8546a4c427e6a077c8dcf88f36d99cc3313db
PedroLabrador <pedrolabrador3@outlook.com>
2018-04-04 23:39:36 -0400

    Selecting file from same folder

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
-import '../main.html';
+import './main.html';
 
 // Template.hello.onCreated(function helloOnCreated() {
 //   // counter starts at 0
